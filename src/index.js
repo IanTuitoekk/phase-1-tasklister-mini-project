d997e33042c854b5b2a554f00d9585d9cdeb5c25
IanTuitoekk <iantuitoek4@gmail.com>
2025-07-02 16:09:00 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  buildToDo(document.getElementById("new-task-description").value);
 });
+
+function buildToDo(todo) {
+  let li = document.createElement("li");
+  li.textContent = todo;
+  document.querySelector("#tasks").appendChild(li);
+}
