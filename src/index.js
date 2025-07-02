4cfb4ee7d95acd3b50eadd5a33ae5d54ed65d405
IanTuitoekk <iantuitoek4@gmail.com>
2025-07-02 16:57:51 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  buildToDo(document.getElementById("new-task-description").value);
+document.addEventListener("DOMContentLoaded", () =>{
+  let form = document.querySelector("form")
+    form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo(document.getElementById("new-task-description").value);
+    form.reset() 
+  })
 });
 
 function buildToDo(todo) {
   let li = document.createElement("li");
   li.textContent = todo;
   document.querySelector("#tasks").appendChild(li);
+
+  let button = document.createElement("button")
+  button.textContent = 'x'
+  button.addEventListener("click",deleteToDo)
+
+  document.querySelector('li').appendChild(button)
+}
+
+function deleteToDo(e){
+  e.target.parentNode.remove()
 }
