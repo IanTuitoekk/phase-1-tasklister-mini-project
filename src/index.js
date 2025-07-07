c6d73afd8eaa6c81548869f21e90b32e1743de63
IanTuitoekk <iantuitoek4@gmail.com>
2025-07-07 18:03:09 +0300

    Added another input field

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
-document.addEventListener("DOMContentLoaded", () =>{
-  let form = document.querySelector("form")
-    form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    buildToDo(document.getElementById("new-task-description").value);
-    form.reset() 
+document.addEventListener("DOMContentLoaded", () => {
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let description = document.getElementById("new-task-description").value
+    let duration = document.getElementById("new-task-duration").value
+    buildToDo(description, duration)
+    form.reset()
   })
-});
+})
 
-function buildToDo(todo) {
-  let li = document.createElement("li");
-  li.textContent = todo;
-  document.querySelector("#tasks").appendChild(li);
+function buildToDo(todo, duration) {
+  let li = document.createElement("li")
+
+  if (duration) {
+    li.textContent = `${todo} - Duration: ${duration} `
+  } else {
+    li.textContent = todo + ' '
+  }
 
   let button = document.createElement("button")
   button.textContent = 'x'
-  button.addEventListener("click",deleteToDo)
+  button.addEventListener("click", deleteToDo)
 
-  document.querySelector('li').appendChild(button)
+  li.appendChild(button)            
+  document.querySelector('#tasks').appendChild(li)
 }
 
-function deleteToDo(e){
-  e.target.parentNode.remove()
+function deleteToDo(e) {
+  e.target.parentNode.remove();
 }
